29dd4816efdad3754dcbb68af98fa2dcb72e26df
Luki120 <lukicassino@hotmail.com>
2021-06-02 23:05:29 -0300

    Changelog View

--- AprilPrefs/APRRootListController.h
+++ AprilPrefs/APRRootListController.h
@@ -1,8 +1,43 @@
 #import <Preferences/PSSpecifier.h>
 #import <Preferences/PSTableCell.h>
 #import <Preferences/PSListController.h>
+#import "APPAnimatedTitleView.h"
 
 
+
+UIBarButtonItem *changelogButtonItem;
+
+
+@interface OBButtonTray : UIView
+@property (nonatomic,retain) UIVisualEffectView * effectView;
+- (void)addButton:(id)arg1;
+- (void)addCaptionText:(id)arg1;;
+@end
+
+@interface OBBoldTrayButton : UIButton
+- (void)setTitle:(id)arg1 forState:(unsigned long long)arg2;
++ (id)buttonWithType:(long long)arg1;
+@end
+
+@interface OBWelcomeController : UIViewController
+@property (nonatomic, retain) UIView *viewIfLoaded;
+@property (nonatomic, strong) UIColor *backgroundColor;
+- (OBButtonTray *)buttonTray;
+- (id)initWithTitle:(id)arg1 detailText:(id)arg2 icon:(id)arg3;
+- (void)addBulletedListItemWithTitle:(id)arg1 description:(id)arg2 image:(id)arg3;
+@end
+
+@interface _UIBackdropView : UIView
+- (id)initWithFrame:(CGRect)arg1 autosizesToFitSuperview:(BOOL)arg2 settings:(id)arg3;
+@property (assign, nonatomic) BOOL blurRadiusSetOnce;
+@property (assign, nonatomic) double _blurRadius;
+@property (nonatomic, copy) NSString * _blurQuality;
+- (id)initWithSettings:(id)arg1;
+@end
+
+@interface _UIBackdropViewSettings : NSObject
++ (id)settingsForStyle:(long long)arg1;
+@end
 
 
 @interface AprilTableCell : PSTableCell
@@ -33,7 +68,10 @@
     UITableView * _table;
 
 }
+@property (nonatomic, retain) OBWelcomeController *changelogController;
 @property (nonatomic, retain) NSMutableDictionary *savedSpecifiers;
+@property (nonatomic, retain) UIBarButtonItem *respringButton;
+@property (nonatomic, strong) APPAnimatedTitleView *titleView;
 @property (nonatomic, retain) UILabel *titleLabel;
 @property (nonatomic, retain) UIView *headerView;
 @property (nonatomic, retain) UIImageView *iconView;

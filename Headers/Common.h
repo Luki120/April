ba7c737bf458daa5a7ed4b007b19e69965cbc399
luki120 <luki120@exerra.xyz>
2022-12-17 00:58:33 -0300

    added constants for notifications

--- Headers/Common.h
+++ Headers/Common.h
@@ -1,5 +1,10 @@
 static NSString *const kPath = @"/var/mobile/Library/Preferences/me.luki.aprilprefs.plist";
 
+static NSNotificationName const AprilApplyImageNotification = @"AprilApplyImageNotification";
+static NSNotificationName const AprilApplyGradientNotification = @"AprilApplyGradientNotification";
+static NSNotificationName const AprilApplyAlphaNotification = @"AprilApplyAlphaNotification";
+static NSNotificationName const AprilPresentVCNotification = @"AprilPresentVCNotification";
+
 #define kAprilTintColor [UIColor colorWithRed:1.0 green:0.55 blue:0.73 alpha: 1.0]
 #define kUserInterfaceStyle UIScreen.mainScreen.traitCollection.userInterfaceStyle == UIUserInterfaceStyleDark
 
@@ -10,8 +15,5 @@ static NSString *const kPath = @"/var/mobile/Library/Preferences/me.luki.aprilpr
 
 @interface _UIBackdropView : UIView
 @property (assign, nonatomic) BOOL blurRadiusSetOnce;
-@property (assign, nonatomic) double _blurRadius;
-@property (copy, nonatomic) NSString *_blurQuality;
-- (id)initWithSettings:(id)arg1;
 - (id)initWithFrame:(CGRect)arg1 autosizesToFitSuperview:(BOOL)arg2 settings:(id)arg3;
 @end
